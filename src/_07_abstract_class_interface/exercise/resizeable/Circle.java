a8f4c7cd34526f1aaa0663c71113070a48283861
tiendat3011 <tiendat30111999@gmail.com>
2022-05-13 09:08:55 +0700

    ok

---
+++ src/_07_abstract_class_interface/exercise/resizeable/Circle.java
@@ -0,0 +1,6 @@
+package _07_abstract_class_interface.exercise.resizeable;
+
+public class Circle implements Resizeable {
+    
+
+}
---
+++ src/_07_abstract_class_interface/exercise/resizeable/Resizeable.java
@@ -0,0 +1,6 @@
+package _07_abstract_class_interface.exercise.resizeable;
+
+public interface Resizeable {
+    public static void resize(double percent) {
+    }
+}
