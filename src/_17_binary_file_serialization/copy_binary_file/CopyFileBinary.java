726a3b72d509de257c87e4311356892ec24d0bc1
tiendat3011 <tiendat30111999@gmail.com>
2022-05-30 20:34:20 +0700

    ok

--- src/_17_binary_file_serialization/copy_binary_file/CopyFileBinary.java
+++ src/_17_binary_file_serialization/copy_binary_file/CopyFileBinary.java
@@ -1,16 +1,29 @@
 package _17_binary_file_serialization.copy_binary_file;
 
+import java.io.*;
 import java.util.List;
 
 public class CopyFileBinary {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        InputStream inStream = null;
+        OutputStream outStream = null;
+
         try {
-            List<Object> stringList = (List<Object>) ReadAndWrite.read("src\\_17_binary_file_serialization\\product_manager_save_binary_file\\ListProductt.txt");
-            ReadAndWrite.write("src\\_17_binary_file_serialization\\copy_binary_file\\2.txt", stringList);
-        } catch (Exception e) {
+            inStream = new FileInputStream(new File("src\\_17_binary_file_serialization\\copy_binary_file\\ListProductt.txt"));
+            outStream = new FileOutputStream(new File("src\\_17_binary_file_serialization\\copy_binary_file\\2.txt"));
+
+            int length;
+            byte[] buffer = new byte[1024];
+
+            while ((length = inStream.read(buffer)) > 0) {
+                outStream.write(buffer, 0, length);
+            }
+        } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            inStream.close();
+            outStream.close();
         }
     }
 }
-
